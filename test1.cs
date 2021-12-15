4b3b4e69db474bc1c771dec13605d6c2284c624f
GeogNum <85230851+G3ogNum@users.noreply.github.com>
2021-12-15 15:27:37 +0800

    随手测试
    无

---
+++ test1.cs
@@ -0,0 +1,15 @@
+using System;
+
+namespace _01调试错误和排除_Debug_
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Now,I am the predator.");
+        }
+    }
+    
+
+}
